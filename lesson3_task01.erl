618ec33cc9b893c5a6fd69b28d49b920889dde40
Angelika Antonenko <luckyhedgehooog@gmail.com>
2023-08-31 00:36:09 +0300

    add task01

---
+++ lesson3_task01.erl
@@ -0,0 +1,11 @@
+-module(lesson3_task02).
+-export([reverse/1, reverse/2, first_word/1, first_word/2]).
+
+reverse(L)->reverse(L,[]).
+reverse([],S)-> S;
+reverse([H|T], S)->reverse(T,[H|S]).
+
+first_word(L) -> first_word(L, []).
+first_word([], S) -> S;
+first_word([32|_], S) -> S;
+first_word([H|T], S) -> first_word(T, [H|S]).
